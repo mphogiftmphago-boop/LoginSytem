fb0d8641cf3c875c20227c2ff94f21423bf988ec
Gift-boop <mphogiftmphago@gmail.com>
2026-09-18 18:03:15 +0200

    seventh commit

--- test/LoginTest.java
+++ test/LoginTest.java
@@ -9,11 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
-/**
- *
- * @author mphog
- */
 import static org.junit.Assert.*;
 import org.junit.Test;
 
